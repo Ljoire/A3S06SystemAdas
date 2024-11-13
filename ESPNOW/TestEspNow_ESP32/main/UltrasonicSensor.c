9eb267e4c530a755c2eeef0411c064a159c4bf6b
LJoire <leon.joire@esme.fr>
2024-11-13 17:09:54 +0100

    Adding ultrasonic sensor v2

--- ESPNOW/TestEspNow_ESP32/main/UltrasonicSensor.c
+++ ESPNOW/TestEspNow_ESP32/main/UltrasonicSensor.c
@@ -1,43 +1,48 @@
-/*
-*   SENSOR CODE 
-*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
-#include "esp_rom_sys.h"
-#include "ultrasonic.h"
 
 
-/* Initialization of the sensor*/
+#define TRIG_PIN GPIO_NUM_5      
+#define ECHO_PIN GPIO_NUM_18    
+#define SOUND_SPEED 0.0343       
+
 void init_ultrasonic_sensor() {
-    gpio_set_direction(TRIG_PIN_1, GPIO_MODE_OUTPUT);
-    gpio_set_direction(ECHO_PIN_1, GPIO_MODE_INPUT);
-    gpio_set_direction(TRIG_PIN_2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(ECHO_PIN_2, GPIO_MODE_INPUT);
+    gpio_set_direction(TRIG_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
 }
 
-/*  measure of the distance for one sensor
-*   INPUT : Pin for triger and echo (int)
-*   OUTPUT: Floating number who represent the distance*/
-float measure_distance(int trig_pin, int echo_pin) {
-    gpio_set_level(trig_pin, 0);
-    esp_rom_delay_us(2);
-    gpio_set_level(trig_pin, 1);
-    esp_rom_delay_us(10);
-    gpio_set_level(trig_pin, 0);
+float measure_distance() {
 
-    int64_t start_wait = esp_timer_get_time();
-    while (gpio_get_level(echo_pin) == 0 && esp_timer_get_time() - start_wait < TIMEOUT);
+    gpio_set_level(TRIG_PIN, 1);
+    esp_rom_delay_us(10);  
+    gpio_set_level(TRIG_PIN, 0);
 
+    while (gpio_get_level(ECHO_PIN) == 0);
     int64_t start_time = esp_timer_get_time();
-    while (gpio_get_level(echo_pin) == 1 && esp_timer_get_time() - start_time < TIMEOUT);
+
+    while (gpio_get_level(ECHO_PIN) == 1);
     int64_t end_time = esp_timer_get_time();
 
     int64_t pulse_duration = end_time - start_time;
-    float distance = (pulse_duration / 2.0) * SOUND_SPEED;
 
-    if (pulse_duration >= TIMEOUT) {
-        return -1;
-    }
+    float distance = (pulse_duration / 2.0) * SOUND_SPEED;
+    printf("Distance: %.2f cm\n", distance);  
 
     return distance;
 }
+
+void ultrasonic_task(void *pvParameters) {
+    while (true) {
+        float distance = measure_distance();
+        vTaskDelay(10000 / portTICK_PERIOD_MS);  
+    }
+}
+
+/* TO implement*/
+//    xTaskCreate(ultrasonic_task, "ultrasonic_task", 2048, NULL, 5, NULL);
--- ESPNOW/TestEspNow_ESP32/main/include/ultrasonic.h
+++ ESPNOW/TestEspNow_ESP32/main/include/ultrasonic.h
@@ -1,12 +1,9 @@
-#define TRIG_PIN_1 4
-#define ECHO_PIN_1 5
-#define TRIG_PIN_2 18
-#define ECHO_PIN_2 19
-#define CRITICAL_DISTANCE_CM 20
-#define SOUND_SPEED 0.034
-#define TIMEOUT 30000
-
+#define TRIG_PIN GPIO_NUM_5      
+#define ECHO_PIN GPIO_NUM_18    
+#define SOUND_SPEED 0.0343       
 
 void init_ultrasonic_sensor(void);
+
+float measure_distance(void);
 
 float measure_distance(int trig_pin, int echo_pin);
