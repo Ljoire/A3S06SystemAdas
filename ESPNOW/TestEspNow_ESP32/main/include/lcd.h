0359b37972cb33b7303e1a4964d061f925988fad
LJoire <leon.joire@esme.fr>
2024-11-16 14:08:45 +0100

    Add source and header file for LCD

---
+++ ESPNOW/TestEspNow_ESP32/main/include/lcd.h
@@ -0,0 +1,23 @@
+
+
+static esp_err_t lcd_write_byte(uint8_t cmd, bool is_data);
+
+
+static void lcd_send_cmd(uint8_t cmd);
+
+
+void lcd_init(void);
+
+
+void lcd_clear(void);
+
+
+void lcd_home(void);
+
+
+void lcd_set_cursor(uint8_t row, uint8_t col);
+
+
+void lcd_print(const char* str);
+
+void lcd_backlight(bool on);
